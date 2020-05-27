4803dd5df97c68fda3cc19e0e3740f699179ca28
sylvie95 <1451932421@qq.com>
2020-05-27 10:52:21 +0800

    Update StaticPeram.java

--- BeiShan/src/main/java/com/beishan/util/StaticPeram.java
+++ BeiShan/src/main/java/com/beishan/util/StaticPeram.java
@@ -12,8 +12,8 @@ public class StaticPeram {
 	final static String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
 	final static String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 	// 替换成你的AK (产品密)
-	final static String accessKeyId = "LTAIYlI8pqDTB65d";// 你的accessKeyId,参考本文档步骤2
-	final static String accessKeySecret = "SpZcH8ptthWBcQbUNtkCkZDRYJCBsS";// 你的accessKeySecret，参考本文档步骤2
+	final static String accessKeyId = "xxxxxxxxxxxxxd";// 你的accessKeyId,参考本文档步骤2
+	final static String accessKeySecret = "SpZcxxxxxxxxxxxxxxxxxxxsS";// 你的accessKeySecret，参考本文档步骤2
 	// 必填:短信签名-可在短信控制台中找到
 	final static String SignName = "北山";
 	// 必填:短信模板-可在短信控制台中找到
